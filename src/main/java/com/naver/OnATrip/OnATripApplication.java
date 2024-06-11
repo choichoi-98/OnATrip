1d4af528fd78c4926ee5cb1a571f9f52d917a1de
jiseon <j2sunnn@gmail.com>
2024-06-11 16:23:11 +0900

    240611 초기 설정 세팅

---
+++ src/main/java/com/naver/OnATrip/OnATripApplication.java
@@ -0,0 +1,13 @@
+package com.naver.OnATrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnATripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnATripApplication.class, args);
+	}
+
+}
