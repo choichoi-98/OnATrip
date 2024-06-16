d35ddc08881e7345429de1ea95440c8ac87fd65b
htakimsean <dustlrka86@gmail.com>
2024-06-16 17:24:13 +0900

    2024/06/16 main front 적용중

--- src/main/java/com/naver/OnATrip/controller/WebController.java
+++ src/main/java/com/naver/OnATrip/controller/WebController.java
@@ -1,12 +1,15 @@
 package com.naver.OnATrip.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+
+    @GetMapping("/main")
+    public String main(Model model) {
+        return "main";
     }
 }
