c1a2bee967796ff2c79ecef6df436b2e4ef1e66b
htakimsean <dustlrka86@gmail.com>
2024-06-19 16:55:32 +0900

    2024/06/19 admin 페이지 작업

--- src/main/java/com/naver/OnATrip/controller/AdminController.java
+++ src/main/java/com/naver/OnATrip/controller/AdminController.java
@@ -3,14 +3,13 @@ package com.naver.OnATrip.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class WebController {
+public class AdminController {
 
-    @GetMapping("/main")
-    public String main(Model model) {
-        return "main";
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        return "admin/adminMain";
     }
 
 
