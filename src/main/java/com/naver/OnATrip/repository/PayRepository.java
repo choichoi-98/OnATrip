95b4c1becfcde1cca49f5f69c5b677a196c628b3
jiseon <j2sunnn@gmail.com>
2024-06-25 17:11:55 +0900

    240625 PaymentService 수정

--- src/main/java/com/naver/OnATrip/repository/PayRepository.java
+++ src/main/java/com/naver/OnATrip/repository/PayRepository.java
@@ -1,13 +1,8 @@
 package com.naver.OnATrip.repository;
 
-import com.naver.OnATrip.entity.Member;
-import com.naver.OnATrip.entity.pay.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Repository
 public class PayRepository {
@@ -19,7 +14,6 @@ public class PayRepository {
         this.em = em;
         this.query = new JPAQueryFactory(em);
     }
-
 
 
 
