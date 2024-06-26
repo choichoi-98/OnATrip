055b1e008e2f6ed49addb4e7072835389c22cad6
choichoi-98 <choichoi980804@gmail.com>
2024-06-26 10:29:36 +0900

    -choichoi-98,plan.java오류수정

--- src/main/java/com/naver/OnATrip/entity/plan/Plan.java
+++ src/main/java/com/naver/OnATrip/entity/plan/Plan.java
@@ -15,8 +15,8 @@ public class Plan {
 
     private Long memberId;//회원가입 기능 완료 시 수정필요
 
-    @ManyToOne
-    @JoinColumn(name = "location_id")
+//    @ManyToOne
+//    @JoinColumn(name = "location_id")
     private String country;//->임시
     //추후에 국가 및 나라 코드 google api에 맞춰서 수정 필요!
     //locationId로 바꿔야 함.
