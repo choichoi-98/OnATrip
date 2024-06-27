8d57cb11d2414fbf5ec94fe4f84b5287debc9126
jiseon <j2sunnn@gmail.com>
2024-06-27 23:05:22 +0900

    240627 결제 페이지 수정

--- src/main/java/com/naver/OnATrip/service/PayService.java
+++ src/main/java/com/naver/OnATrip/service/PayService.java
@@ -1,9 +1,14 @@
 package com.naver.OnATrip.service;
 
+import com.naver.OnATrip.entity.pay.PrePaymentEntity;
 import com.siot.IamportRestClient.IamportClient;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.PrepareData;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
 
 @Service
 @Transactional(readOnly = true)
@@ -23,5 +28,10 @@ public class PayService {
         return iamportClient;
     }
 
-
+//    public void postPrepare(PrePaymentEntity request) throws IamportResponseException, IOException {
+//        PrepareData prepareData = new PrepareData(request.getMerchantUid(), request.getAmount());
+//        iamportClient.postPrepare(prepareData);  // 사전 등록 API
+//
+//        //prePaymentRepository.save(request); // 주문번호와 결제예정 금액 DB 저장
+//    }
 }
