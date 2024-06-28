a2989e16adcdf6547c815ecb16e4997e870e6e5d
htakimsean <dustlrka86@gmail.com>
2024-06-28 17:13:52 +0900

    2024/06/27 admin 여행지 추가 ajax 작업 중(두 번 데이터 들어가는거 고쳐야함)

--- src/main/java/com/naver/OnATrip/repository/admin/LocationRepository.java
+++ src/main/java/com/naver/OnATrip/repository/admin/LocationRepository.java
@@ -1,17 +1,9 @@
 package com.naver.OnATrip.repository.admin;
 
 import com.naver.OnATrip.entity.Location;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class LocationRepository {
-
-    public interface LoactionRepository extends JpaRepository<Location, Long> {
-    }
-
-    //여행지 추가
-    public void addLocation(Location location) {
-    }
+public interface LocationRepository extends JpaRepository<Location, Long> {
 }
---
+++ src/main/java/com/naver/OnATrip/service/LocationService.java
@@ -0,0 +1,40 @@
+package com.naver.OnATrip.service;
+
+import com.naver.OnATrip.entity.Location;
+import com.naver.OnATrip.repository.admin.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+@Service
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    @Autowired
+    public LocationService(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+
+    @Transactional
+    //여행지 추가
+    public void addLocation(String country, String countryCode, String city, String description, MultipartFile file, String locationType) throws IOException {
+        // 파일 유효성 검사
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("이미지 파일을 업로드해야 합니다.");
+        }
+
+        Location location = new Location();
+        location.setCountryName(country);
+        location.setCountryCode(countryCode);
+        location.setCity(city);
+        location.setDescription(description);
+        location.setImage(file.getBytes()); // 이미지를 byte 배열로 변환하여 설정
+
+        // 여행지 저장
+        locationRepository.save(location);
+    }
+}
