c5807687446e44b266f08e466379dbba80973bb0
choichoi-98 <choichoi980804@gmail.com>
2024-06-28 17:13:19 +0900

    -choichoi-98,addroute 백단 작업 중, 6/28/금/17:00

--- src/main/java/com/naver/OnATrip/entity/plan/DetailPlan.java
+++ src/main/java/com/naver/OnATrip/entity/plan/DetailPlan.java
@@ -1,13 +1,17 @@
 package com.naver.OnATrip.entity.plan;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetailPlan {
 
     @Id
--- src/main/java/com/naver/OnATrip/entity/plan/Route.java
+++ src/main/java/com/naver/OnATrip/entity/plan/Route.java
@@ -2,11 +2,11 @@ package com.naver.OnATrip.entity.plan;
 
 import com.naver.OnATrip.constant.RouteCategory;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@Builder
 public class Route {
 
     @Id
@@ -15,21 +15,35 @@ public class Route {
 
     @ManyToOne
     @JoinColumn(name = "detailPlan_id")
-    private DetailPlan detailPlan;
+    private DetailPlan detailPlan_id;
 
     @Enumerated(EnumType.STRING)
-    private RouteCategory category;//장소,메모
+    private RouteCategory category; // 장소, 메모
 
-    private int day_number;//여행 일차(1일차, 2일차...)
+    private int day_number; // 여행 일차(1일차, 2일차...)
 
-    private String place_name;//장소명
+    private String place_name; // 장소명
 
-    private String memo;//메모
+    private String memo; // 메모
 
     private String address;
 
-    private String lat;//위도
+    private String lat; // 위도
 
-    private String lon;//경도
+    private String lon; // 경도
 
+    public Route(Long id, DetailPlan detailPlan_id, RouteCategory category, int day_number, String place_name, String memo, String address, String lat, String lon) {
+        this.id = id;
+        this.detailPlan_id = detailPlan_id;
+        this.category = category;
+        this.day_number = day_number;
+        this.place_name = place_name;
+        this.memo = memo;
+        this.address = address;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public Route() {
+    }
 }
