a974257d3f70c8803e603f04eb10accbe69c3d76
htakimsean <dustlrka86@gmail.com>
2024-07-02 00:05:27 +0900

    2024/07/01 LocationService 수정

--- src/main/java/com/naver/OnATrip/service/LocationService.java
+++ src/main/java/com/naver/OnATrip/service/LocationService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+
 @Service
 public class LocationService {
 
